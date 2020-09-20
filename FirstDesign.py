5f0f150e1782ad27f810bc4453b18433a0198567
Sam Walkow <samwalkow@gmail.com>
2020-09-20 15:28:37 -0500

    some initial thoughts

---
+++ FirstDesign.py
@@ -0,0 +1,63 @@
+
+# board as dict?
+
+board = {}
+
+for i in range(1, 17):
+    if i not in board:
+        board[i] = ""
+print(board)
+
+for tile in range(len(board)):
+    tile =
+
+
+class Tile:
+
+    def __init__(self, position, move):
+        self.position =position
+        self.move = move
+
+class GameState:
+
+    def __init__(self, board_status, turn_number, player):
+        self.board_status = board_status
+        self.turn_number = turn_number
+        self.player = player
+        self.legal_next_move = []
+
+class Node:
+
+    def __init__(self, move: int, game_state='GameState'):
+        self.move = move
+        self.game_state = game_state
+        self.parent = None
+        self.children = []
+
+    def append_child(self, child: "Node"):
+        self.children.append(child)
+        child.parent = self
+
+
+def final_move():
+    # conditions for a win, draw, and loss
+    # if there are no consecutive spaces left
+    print("game over")
+
+def validate_moves():
+    # make sure the move is possible
+    # is that board not full?
+    # that local spot taken?
+    print("validate")
+
+def generate_next_move():
+    # keep track of last move
+    print("new move")
+
+def matchboxes():
+    matchbox = list()
+    bead = str
+    print("learning with matchboxes")
+
+def output():
+    print("Finished Game")
---
+++ Lecture4.py
@@ -0,0 +1,48 @@
+## create tree
+
+# create class for each node in the tree, not whole tree
+# all trees are graphs, no loops (can't be cyclical), directed, top down
+# can typicallly apply graph functionality to a tree
+# game tree - start move - how many legal turns does player one have? Each move creates a "game state" (what is on the board and where, and any off board counters, score, what turn is it, who turn is it next, ), 
+# memory saving - traverse up the tree and only store latest move, or store x and y coords in tuple as a dict key and maintain list values
+
+# homework re-create matchbox tictactoe with randomly generated moves (with custom weighting, depending on each number of colored beads), also store each legal, possible moves
+
+# simulating random choices with weights
+from random import choices
+
+# for x in range(10):
+#     print(choices(['red', 'blue', 'green'], weights=[1, 1, 3]))
+
+class GameState:
+    
+    def __init__(self, board_status, turn_number, player):
+        self.board_status = board_status
+        self.turn_number = turn_number
+        self.player = player
+        self.legal_next_move = []
+
+class Node:
+
+    def __init__(self, move: int, game_state = 'GameState'):
+        self.move = move
+        self.game_state = game_state
+        self.parent = None
+        self.children = []
+        
+    def append_child(self, child: 'Node'):
+        self.children.append(child)
+        child.parent = self
+
+
+x = Node(5)
+
+x.append_child(Node(2))
+x.append_child(Node(3))
+
+x.children[1].append_child(Node(82))
+
+print(x.value, [print(i.value) for i in x.children])
+
+
+
