44ad41ccd4d0da3348a47cd56c27744aa2c5dfd0
Vel Wu <ericwu102@yahoo.com.tw>
2020-09-28 17:50:56 -0500

    Some basic game states and how moves are parsed

--- A5_prototype/HTL_vel_game_rules_n_misc_draft.py
+++ A5_prototype/HTL_vel_game_rules_n_misc_draft.py
@@ -13,25 +13,30 @@ empty_board_numpy = np.array([
 
 #TODO: Build a game rule on this data structure:
 # It is assumed that everything in the game_state is a legal move
-game_state_example = {"PlayerA" : [[(0,0), (0,1), (0,2), (0,3)], [(2,0), (2,1), (2,2), (2,3)]],
-                      "PlayerB" : [[(1,0), (1,1), (1,2), (1,3)], [(3,0), (3,1), (3,2), (3,3)]]
+game_state_example_1 = {"Lines" : [(0,0), (0,1), (0,2), (0,3), (1,2)]
               }
+game_state_example_2 = {"Lines" : []}
 
-empty_board = [["-", "-", "-", "-"],
-               ["-", "-", "-", "-"],
-               ["-", "-", "-", "-"],
-               ["-", "-", "-", "-"]]
-
-def visualize_game(board, game_state):
-    for each_row in game_state["PlayerA"]:
-        for each_col_value in each_row:
-            board[each_col_value[0]][each_col_value[1]] = "A"
-    for each_row in game_state["PlayerB"]:
-        for each_col_value in each_row:
-            board[each_col_value[0]][each_col_value[1]] = "B"
+def visualize_game(game_state):
+    board_for_printing = [["-", "-", "-", "-"],
+                          ["-", "-", "-", "-"],
+                          ["-", "-", "-", "-"],
+                          ["-", "-", "-", "-"]]
+    for each_col_value in game_state["Lines"]:
+        board_for_printing[each_col_value[0]][each_col_value[1]] = "*"
     iterator = 0
-    for i in range(0, len(board)):
-        print(*board[iterator])
+    for i in range(0, len(board_for_printing)):
+        print(*board_for_printing[iterator])
         iterator += 1
 
-visualize_game(empty_board, game_state_example)
+def make_a_move_from_input(game_state, move_syntax):
+    move_parsed = eval(move_syntax)
+    for each in enumerate(move_parsed):
+        print(each[1])
+        if each[1] not in game_state["Lines"]:
+            game_state["Lines"].append(each[1])
+    print(game_state)
+    visualize_game(game_state)
+
+#visualize_game(game_state_example_1)
+make_a_move_from_input(game_state_example_2, "(0,0),(2,1)")
