f429b29cf88b8d4da24c5631c3005cc6fe4f6928
Vel Wu <ericwu102@yahoo.com.tw>
2020-09-30 22:26:36 -0500

    Making moves using string inputs like "(0,0)(2,1)"

--- A5_prototype/HTL_vel_game_rules_n_misc_draft.py
+++ A5_prototype/HTL_vel_game_rules_n_misc_draft.py
@@ -34,12 +34,53 @@ from datetime import datetime
 #References used:
 # - https://stackoverflow.com/questions/3838329/how-can-i-check-if-two-segments-intersect
 # - https://bryceboe.com/2006/10/23/line-segment-intersection-algorithm/
+# - https://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/
+# - https://stackoverflow.com/questions/328107/how-can-you-determine-a-point-is-between-two-other-points-on-a-line-segment
 def counter_clockwise(A,B,C):
     return (C[0]-A[0]) * (B[1]-A[1]) > (B[0]-A[0]) * (C[1]-A[1])
 
+def Area2(A, B, C):
+    return (B[1] - A[1]) * (C[0] - A[0]) - (C[1] - A[1]) * (B[0] - A[0])
+
+def IsOnLeft(A, B, C):
+    return Area2(A, B, C) > 0
+
+def IsOnRight(A, B, C):
+    return Area2(A, B, C) < 0
+
+def IsCollinear(A, B, C):
+    return Area2(A, B, C) == 0
+
+def IsOverlapping(A, B, C):
+    #Line AB is the Anchor
+    crossproduct = (C[0] - A[0]) * (B[1] - A[1]) - (C[1] - A[1]) * (B[0] - A[0])
+    if abs(crossproduct) != 0:
+        return False
+
+    dotproduct = (C[1] - A[1]) * (B[1] - A[1]) + (C[0] - A[0]) * (B[0] - A[0])
+    if dotproduct < 0:
+        return False
+
+    squaredlengthba = (B[1] - A[1]) * (B[1] - A[1]) + (B[0] - A[0]) * (B[0] - A[0])
+    if dotproduct > squaredlengthba:
+        return False
+
+    return True
+
 # Return true if line segments AB and CD intersect
 def intersect(A,B,C,D):
-    return counter_clockwise(A,C,D) != counter_clockwise(B,C,D) and counter_clockwise(A,B,C) != counter_clockwise(A,B,D)
+    #return counter_clockwise(A, C, D) != counter_clockwise(B, C, D) \
+    #       and counter_clockwise(A, B, C) != counter_clockwise(A, B, D)
+    if IsOnLeft(A,B,C) and IsOnRight(A,B,D):
+        return True
+    elif IsOnLeft(A,B,D) and IsOnRight(A,B,C):
+        return True
+    elif IsOverlapping(A,B,D) or IsOverlapping(B, D, A):
+        return True
+
+    return False
+
+
 
 def make_a_move_randomly(game_state):
     game_state
