9dd7e5fb9798d63cec01d681d4ab0b66a3f20093
jrajath94 <jrajath94@gmail.com>
2020-10-01 19:38:09 -0500

    ++ Generate Game state randomly

--- HTL_game_rules_n_misc.py
+++ HTL_game_rules_n_misc.py
@@ -9,7 +9,7 @@ from datetime import datetime
 
 # one line drawn by any player is represented as: [(0,0), (0,1), (0,2), (0,3)], [(2,0), (2,1), (2,2), (2,3)], etc.
 
-#TODO: Build a game rule on this data structure:
+# TODO: Build a game rule on this data structure:
 # It is assumed that everything in the game_state is a legal move
 
 # For learning: "(0,0),(2,1)" : 50
@@ -31,28 +31,33 @@ from datetime import datetime
 # is exactly what we are expected to expand on
 
 
-#References used:
+# References used:
 # - https://stackoverflow.com/questions/3838329/how-can-i-check-if-two-segments-intersect
 # - https://bryceboe.com/2006/10/23/line-segment-intersection-algorithm/
 # - https://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/
 # - https://stackoverflow.com/questions/328107/how-can-you-determine-a-point-is-between-two-other-points-on-a-line-segment
-def counter_clockwise(A,B,C):
-    return (C[0]-A[0]) * (B[1]-A[1]) > (B[0]-A[0]) * (C[1]-A[1])
+def counter_clockwise(A, B, C):
+    return (C[0] - A[0]) * (B[1] - A[1]) > (B[0] - A[0]) * (C[1] - A[1])
+
 
 def Area2(A, B, C):
     return (B[1] - A[1]) * (C[0] - A[0]) - (C[1] - A[1]) * (B[0] - A[0])
 
+
 def IsOnLeft(A, B, C):
     return Area2(A, B, C) > 0
+
 
 def IsOnRight(A, B, C):
     return Area2(A, B, C) < 0
 
+
 def IsCollinear(A, B, C):
     return Area2(A, B, C) == 0
 
+
 def IsOverlapping(A, B, C):
-    #Line AB is the Anchor
+    # Line AB is the Anchor
     crossproduct = (C[0] - A[0]) * (B[1] - A[1]) - (C[1] - A[1]) * (B[0] - A[0])
     if abs(crossproduct) != 0:
         return False
@@ -67,15 +72,16 @@ def IsOverlapping(A, B, C):
 
     return True
 
+
 # Return true if line segments AB and CD intersect
-def intersect(A,B,C,D):
-    #return counter_clockwise(A, C, D) != counter_clockwise(B, C, D) \
+def intersect(A, B, C, D):
+    # return counter_clockwise(A, C, D) != counter_clockwise(B, C, D) \
     #       and counter_clockwise(A, B, C) != counter_clockwise(A, B, D)
-    if IsOnLeft(A,B,C) and IsOnRight(A,B,D):
+    if IsOnLeft(A, B, C) and IsOnRight(A, B, D):
         return True
-    elif IsOnLeft(A,B,D) and IsOnRight(A,B,C):
+    elif IsOnLeft(A, B, D) and IsOnRight(A, B, C):
         return True
-    elif B == C and (IsOverlapping(A,B,D) or IsOverlapping(B, D, A)):
+    elif B == C and (IsOverlapping(A, B, D) or IsOverlapping(B, D, A)):
         return True
     elif A == C and IsOverlapping(A, D, B):
         return True
@@ -83,11 +89,32 @@ def intersect(A,B,C,D):
     return False
 
 
+game_state_example_1 = {
+    "Lines": [(0, 0), (1, 0), (2, 0), (2, 1), (3, 1), (3, 2), (2, 2), (2, 3), (1, 3), (1, 2), (0, 2), (1, 1)],
+    "Weights": []
+    # Without learning, the 'Weights' start out empty. Might need to loop through it to assign them.
+}
+
+#print("Test inter", intersect((1, 1), (0, 1), (3, 2), (3, 1)))
+board_coordinates = [(x, y) for x in range(4) for y in range(0, 4)]
+from random import shuffle
+
 
 def make_a_move_randomly(game_state):
-    game_state
-    #TODO: list(legal_moves)
-    #TODO: random.choice(list)
+    possible_coordinates = [x for x in board_coordinates if x not in game_state['Lines']]
+    shuffle(possible_coordinates)
+    intersection = False
+    for coordinates in possible_coordinates:
+        intersection = False
+        for p1, p2 in zip(game_state["Lines"][::-1][:-1], game_state["Lines"][::-1][1:]):
+            intersection = intersect(game_state["Lines"][-1], coordinates, p1, p2)
+            if intersection == True:
+                break
+        if intersection == False:
+            game_state["Lines"].append(coordinates)
+            return game_state
+    print("No Valid moves from present state")
+    return game_state
 
 
 def make_a_move_from_input(game_state, move_syntax):
@@ -99,10 +126,11 @@ def make_a_move_from_input(game_state, move_syntax):
         game_state["Lines"].append(move_parsed[1])
         return game_state, move_syntax
 
-    #if move_parsed not in game_state["Lines"]:
+    # if move_parsed not in game_state["Lines"]:
     if move_parsed[0] == game_state["Lines"][0] and move_parsed[1] not in game_state["Lines"]:
         for ele_idx, ele_value in enumerate(game_state["Lines"][:-1]):
-            if intersect(game_state["Lines"][ele_idx], game_state["Lines"][ele_idx + 1], game_state["Lines"][0],move_parsed[1]):
+            if intersect(game_state["Lines"][ele_idx], game_state["Lines"][ele_idx + 1], game_state["Lines"][0],
+                         move_parsed[1]):
                 print("INVALID MOVE: Intersecting lines!:",
                       "Line*", game_state["Lines"][ele_idx], game_state["Lines"][ele_idx + 1], "*",
                       "crossed with Line *", game_state["Lines"][0], move_parsed[1], "*")
@@ -112,9 +140,10 @@ def make_a_move_from_input(game_state, move_syntax):
 
     elif move_parsed[0] == game_state["Lines"][-1] and move_parsed[1] not in game_state["Lines"]:
         for ele_idx, ele_value in enumerate(game_state["Lines"][:-1]):
-            if intersect(game_state["Lines"][ele_idx], game_state["Lines"][ele_idx+1], game_state["Lines"][-1], move_parsed[1]):
+            if intersect(game_state["Lines"][ele_idx], game_state["Lines"][ele_idx + 1], game_state["Lines"][-1],
+                         move_parsed[1]):
                 print("INVALID MOVE: Intersecting lines!:",
-                      "Line*", game_state["Lines"][ele_idx], game_state["Lines"][ele_idx+1], "*",
+                      "Line*", game_state["Lines"][ele_idx], game_state["Lines"][ele_idx + 1], "*",
                       "crossed with Line *", game_state["Lines"][-1], move_parsed[1], "*")
                 return game_state, move_syntax
         print("LEGAL MOVE: At tail endpoint")
@@ -127,16 +156,16 @@ def make_a_move_from_input(game_state, move_syntax):
     # print(game_state)
     # visualize_game(game_state)
 
+
 def human_player_move():
-    #TODO: Complete this function
+    # TODO: Complete this function
     print("Please input your moves in the form of '(x1, y1), (x2, y2)'")
     player_input = input()
     return player_input
 
 
-
 def visualize_game(game_state):
-    #TODO: Determine whether this thing is needed at all.
+    # TODO: Determine whether this thing is needed at all.
     board_for_printing = [["-", "-", "-", "-"],
                           ["-", "-", "-", "-"],
                           ["-", "-", "-", "-"],
